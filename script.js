a5df50e90e6f4a56de6ce8f108fc1d2813074edc
JoaoVitor2004 <joaovitorsouzaa28122004@gmail.com>
2024-05-30 17:45:29 -0300

    novas alterações no javascript

--- script.js
+++ script.js
@@ -13,27 +13,21 @@ const data = new Date()
 const dia = data.getDate()
 const mes = data.getMonth() + 1
 const ano = data.getFullYear()
+console.log(`Data atual: ${dia}/${mes}/${ano}`)
 
-botao.addEventListener('click', function() {
+botao.addEventListener('click', () => {
 
     if (txtDia.value <= 0 || txtDia.value > 31 || txtMes.value <= 0 || txtMes.value > 12 || txtAno.value <= 0 || txtAno.value > ano ) {
 
-        const etiquetas = [label1, label2, label3].forEach((etiqueta) => {
-            etiqueta.style.color = 'red'
-        })
+        const etiquetas = [label1, label2, label3].forEach((etiquetas) => etiquetas.style = 'color: red;')
         const erros = [erro1, erro2, erro3].forEach((erros) => {
-            erros.classList.toggle('hide')
+            erros.classList.remove('hide')
             erros.style.color = 'red'
         })
         
     } else {
-
-        const etiquetas = [label1, label2, label3].forEach((etiqueta) => {
-            etiqueta.style.color = 'black'
-        })
-        const erros = [erro1, erro2, erro3].forEach((erros) => {
-            erros.classList.add('hide')
-        })
+        const etiquetas = [label1, label2, label3].forEach((etiquetas) => etiquetas.style = 'color: black;')
+        const erros = [erro1, erro2, erro3].forEach((erros) => erros.classList.add('hide'))
         let converterDia = Number(txtDia.value)
         let converterMes = Number(txtMes.value)
         let converterAno = Number(txtAno.value)
@@ -43,9 +37,9 @@ botao.addEventListener('click', function() {
 
         const subtrairAno = ano - converterAno
         const subtrairMes = converterMes - mes
-        const subtrairDia = converterDia - dia
+        const subtrairDia = dia - converterDia 
 
-        res1.innerHTML = subtrairAno
+        res1.innerHTML = subtrairAno - 1
         res2.innerHTML = subtrairMes
         res3.innerHTML = subtrairDia
 
